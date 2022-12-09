7d6074a3fd3e80c8e35e8d4642fa59aaa5f94aa4
Danthuluri sai hemanth varma <119394989+danthulurisaihemanth@users.noreply.github.com>
2022-12-09 23:02:54 +0530

    Update python project 6.py

--- python project 6.py
+++ python project 6.py
@@ -3,7 +3,7 @@
 user_input = str(input("Enter a Sentence: "))
 # making list of words
 text = user_input.split()
-# Creating empty string
+# Creating empty string#eth
 a = " "
 # Acessing each word one by one using for loop
 for i in text:
